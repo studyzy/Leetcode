6fcbdde602ee95dc24e16d1f08b7ab868126658d
Devin <studyzy@gmail.com>
2015-03-25 15:13:01 -0700

    add new exam SearchMatrix

---
+++ CSharp/LeetCode/SearchMatrix.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    public partial class Solution
+    {
+        /// <summary>
+        /// https://leetcode.com/problems/search-a-2d-matrix/
+        /// </summary>
+        /// <returns></returns>
+        public bool SearchMatrix(int[,] matrix, int target)
+        {
+            var nrow = matrix.GetLength(0);
+            var ncol = matrix.GetLength(1);
+            for (var i = 0; i < nrow; i++)
+            {
+                if (matrix[nrow - i - 1,0] <= target)
+                {
+                    for (var j = 0; j < ncol; j++)
+                    {
+                        if (matrix[nrow - i - 1, j] == target)
+                            return true;
+                    }
+                    return false;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ CSharp/LeetCodeUT/SearchMatrixUT.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Configuration;
+using System.Text;
+using NUnit.Framework;
+
+namespace LeetCodeUT
+{
+    internal class SearchMatrixUT : BaseUT
+    {
+        [TestCase(11, true)]
+        [TestCase(12, false)]
+        [TestCase(0, false)]
+        [TestCase(-3, false)]
+        public void Test1(int target, bool result)
+        {
+            int[,] matrix = new[,]
+            {
+                {1, 3, 5, 7},
+                {10, 11, 16, 20},
+                {23, 30, 34, 50}
+            };
+            Assert.AreEqual(Solution.SearchMatrix(matrix, target), result);
+
+        }
+    }
+}
