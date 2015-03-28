c0b8c9a1156f9d732918e472d2248ccb8b085903
Devin <studyzy@gmail.com>
2015-03-27 22:31:01 -0700

    * PascalsTriangle.cs: * LeetCodeUT.csproj: * PascalsTriangleUT.cs:
    * LeetCode.csproj: First time to use Xamarin Studio to code.

---
+++ CSharp/LeetCode/PascalsTriangle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+namespace LeetCode
+{
+	public partial class Solution {
+		/// <summary>
+		/// https://leetcode.com/problems/pascals-triangle/
+		/// </summary>
+		/// <param name="numRows">Number rows.</param>
+		public IList<IList<int>> Generate(int numRows) {
+			IList<IList<int>> result = new List<IList<int>> ();
+			if (numRows == 0) {
+				return result;
+			}
+			IList<int> row1 = new List<int> ();
+			row1.Add (1);
+			result.Add (row1);//initial first row is 1
+			for (var i = 2; i <= numRows; i++) {
+				IList<int> row2 = new List<int> ();
+				row2.Add (1);//left one is 1
+				for (var j = 0; j < i - 2; j++) {
+					int n = row1 [j] + row1 [j + 1];
+					row2.Add (n);
+				}
+				row2.Add (1);// last one is 1
+				row1=row2;
+				result.Add (row1);
+			}
+			return result;
+		}
+	}
+}
+
---
+++ CSharp/LeetCodeUT/PascalsTriangleUT.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NUnit.Framework;
+using System.Diagnostics;
+
+namespace LeetCodeUT
+{
+	public class PascalsTriangleUT:BaseUT
+	{
+		[Test]
+		public void Test ()
+		{
+			var list=Solution.Generate (5);
+			foreach (var row in list) {
+				Debug.WriteLine (string.Join (",", row));
+			}
+		}
+	}
+}
+
