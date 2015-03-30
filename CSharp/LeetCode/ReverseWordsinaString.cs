5aca157f296bf1043e4b9130ab32d2827db3727a
Devin <studyzy@gmail.com>
2015-03-30 01:14:11 -0700

    add new very easy exam

---
+++ CSharp/LeetCode/ReverseWordsinaString.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace LeetCode
+{
+	public partial class Solution {
+		public string ReverseWords(string s) {
+			var array=s.Split(new[]{' '},StringSplitOptions.RemoveEmptyEntries);
+			var sb = new System.Text.StringBuilder ();
+			int len = array.Length;
+			for (int i = 0; i < len; i++) {
+				sb.Append (array [len - i - 1]);
+				if (i != len - 1) {
+					sb.Append (" ");
+				}
+			}
+			return sb.ToString ();
+		}
+	}
+}
+
---
+++ CSharp/LeetCodeUT/ReverseWordsinaStringUT.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace LeetCodeUT
+{
+	public class ReverseWordsinaStringUT:BaseUT
+	{
+		[TestCase("the sky is blue","blue is sky the")]
+		public void Test (string input,string output)
+		{
+			var result=Solution.ReverseWords (input);
+			Assert.AreEqual (output, result);
+		}
+	}
+}
+
