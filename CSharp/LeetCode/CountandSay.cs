8c56107523de92d80bb321548f32a2f599792d2d
Devin <studyzy@gmail.com>
2015-04-01 16:56:46 -0700

    add count and say

---
+++ CSharp/LeetCode/CountandSay.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    public partial class Solution
+    {
+        /// <summary>
+        /// https://leetcode.com/problems/count-and-say/
+        /// 递归模拟
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public String CountAndSay(int n)
+        {
+            if (n == 1)
+            {
+                return "1";
+            }
+            var prev = CountAndSay(n - 1);
+            int count = 1;
+            char prevChar = prev[0];
+            if (prev.Length == 1)
+            {
+                return count.ToString() + prevChar;
+            }
+            StringBuilder sb=new StringBuilder();
+            for (int i = 1; i < prev.Length; i++)
+            {
+                var c = prev[i];
+                if (c == prevChar)
+                {
+                    count++;
+                }
+                else
+                {
+                    sb.Append(count.ToString() + prevChar);
+                    count = 1;
+                    prevChar = c;
+                }
+                if (i == prev.Length - 1)//last one
+                {
+                    sb.Append(count.ToString() + c);
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ CSharp/LeetCodeUT/CountAndSayUT.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace LeetCodeUT
+{
+    class CountAndSayUT:BaseUT
+    {
+        [TestCase(1,"1")]
+        [TestCase(2,"11")]
+        [TestCase(3,"21")]
+        [TestCase(4,"1211")]
+        [TestCase(5, "111221")]
+        public void Test(int n,string result)
+        {
+           var str =Solution.CountAndSay(n);
+            Assert.AreEqual(str,result);
+        }
+    }
+}
