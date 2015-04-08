4800d67cb7052593910c521caa9a4eb00d206a73
Devin <studyzy@gmail.com>
2015-04-08 09:58:37 -0700

    not complate

---
+++ CSharp/LeetCode/SwapNodesinPairs.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    public partial class Solution
+    {
+        public ListNode SwapPairs(ListNode head)
+        {
+            //int count = 0;
+            //ListNode p = head;
+            //ListNode result;
+            //while (p.next!=null)
+            //{
+            //    if (count%2 == 0)
+            //    {
+            //        var pn = p.next.next;
+            //        p.next.next = p;
+            //        p.next = pn;
+            //    }
+            //    if (count == 0)
+            //    {
+            //        result=
+            //    }
+            //    count++;
+
+            //}
+            return null;
+        }
+    }
+}
