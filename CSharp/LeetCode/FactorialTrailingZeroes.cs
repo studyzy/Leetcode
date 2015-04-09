2d8a791747abfa08587e6d89d0fac6eb34f17628
Devin <studyzy@gmail.com>
2015-04-09 15:10:27 -0700

    add factorial test

---
+++ CSharp/LeetCode/FactorialTrailingZeroes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    public partial class Solution
+    {
+        /// <summary>
+        /// https://leetcode.com/problems/factorial-trailing-zeroes/
+        /// 计算5的个数，10里面包含1个5，25里面包含2个5
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public int TrailingZeroes(int n)
+        {
+            int ret = 0;
+            long baseNum = 5;//n数字大了会溢出
+            while (n >= baseNum)
+            {
+                ret += n / (int)baseNum;
+                baseNum *= 5;
+            }
+            return ret;  
+        }
+    }
+}
---
+++ CSharp/LeetCodeUT/FactorialTrailingZeroesUT.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace LeetCodeUT
+{
+    class FactorialTrailingZeroesUT:BaseUT
+    {
+        [TestCase(0,0)]
+        [TestCase(1,0)]
+        [TestCase(5,1)]
+        [TestCase(10,2)]
+        [TestCase(30,7)]
+        [TestCase(50, 12)]
+        [TestCase(100,24)]
+        [TestCase(2147483647, 536870902)]
+        public void Test(int n, int count)
+        {
+            Assert.AreEqual(Solution.TrailingZeroes(n),count);
+        }
+        [TestCase(30)]
+        [TestCase(50)]
+        public void TestCalcuate(int n)
+        {
+            Debug.WriteLine(Factorial(n));
+        }
+
+        private string Factorial(int n)
+        {
+            if (n == 1)
+            {
+                return "1";
+            }
+           
+            var n1 = Factorial(n - 1);
+           
+            var result= Solution.Multiply(n.ToString(),n1);
+             if (n%10 == 5 || n%10 == 0)
+            {
+                Debug.WriteLine(n+":"+result);
+            }
+            return result;
+        }
+    }
+}
