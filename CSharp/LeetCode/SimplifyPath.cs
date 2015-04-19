3b98f315761e4de05a3c050ea8803c1f117383e5
Devin <studyzy@gmail.com>
2015-04-18 18:01:39 -0700

    add a simple path test

---
+++ CSharp/LeetCode/SimplifyPath.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+	public partial class Solution 
+	{
+		/// <summary>
+		/// https://leetcode.com/problems/simplify-path/
+		/// 用栈实现，最后把栈弹出组成字符串即可。
+		/// 注意细节，如果是根目录，返回"/"而不是空字符串。
+		/// </summary>
+		/// <returns>The path.</returns>
+		/// <param name="path">Path.</param>
+		public string SimplifyPath(string path) 
+		{
+			if (path == "" || path == "/")
+			{
+				return "/";
+			}
+			Stack<string> stack = new Stack<string> ();
+			var folders = path.Split ('/');
+			foreach (var folder in folders)
+			{
+				if (folder == "..")
+				{
+					if (stack.Count > 0)
+					{
+						stack.Pop ();
+					}
+					continue;
+				}
+				if (folder == "." || folder == "")
+				{
+					continue;
+				}
+				stack.Push (folder);
+			}
+			string str = "";
+			while (stack.Count > 0)
+			{
+				str = "/"+stack.Pop () + str;
+			}
+			if (str == "")
+			{
+				str="/";
+			}
+			return str;
+		}
+	}
+}
+
---
+++ CSharp/LeetCodeUT/SimplifyPathUT.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace LeetCodeUT
+{
+	public class SimplifyPathUT:BaseUT
+	{
+		[TestCase("/a/./b/../../c/","/c")]
+		[TestCase("/a/./b//c/","/a/b/c")]
+		public void Test (string path,string simple)
+		{
+			Assert.AreEqual (Solution.SimplifyPath (path), simple);
+		}
+	}
+}
+
