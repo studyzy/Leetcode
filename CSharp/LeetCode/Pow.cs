43ae7950763d5086d80acb9a18171e1fbf5ac4e4
Devin <studyzy@gmail.com>
2015-04-20 00:22:10 -0700

    add Pow()

---
+++ CSharp/LeetCode/Pow.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace LeetCode
+{
+	public partial class Solution 
+	{
+		/// <summary>
+		/// https://leetcode.com/problems/powx-n/
+		/// 细节题，考虑1，0，-1这几个特殊的x
+		/// </summary>
+		/// <param name="x">The x coordinate.</param>
+		/// <param name="n">N.</param>
+		public double Pow(double x, int n)
+		{
+			if (n == 0)
+			{
+				return 1;
+			}
+			if (n < 0)
+			{
+				x = 1 / x;
+			}
+			bool negative = false;
+			if (x < 0)
+			{
+				negative = true;
+				x = -x;
+			}
+			if (negative)
+			{
+				negative = n % 2 == 0 ? false : true;
+			}
+			double result = x;
+			for (var i = 1; i <Math.Abs( (double)n); i++)
+			{
+				result *= x;
+				if (result == 0.0||result==1)
+					return negative?- result:result;
+			}
+			return negative?- result:result;
+
+		}
+	}
+}
+
---
+++ CSharp/LeetCodeUT/PowUT.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace LeetCodeUT
+{
+	public class PowUT:BaseUT
+	{
+		[TestCase(2,2)]
+		[TestCase(2.1,-2)]
+		[TestCase(2.2,10)]
+		[TestCase(0.0001,9999999)]
+		[TestCase(1.0001,9999999)]
+		[TestCase(-1,9999999)]
+		[TestCase(1,999999)]
+		[TestCase(-13.62608, 3)]
+		public void Test (double x,int n)
+		{
+			var result=Solution.Pow (x, n);
+			var sysresult = Math.Pow (x, n);
+			Assert.AreEqual (result, sysresult);
+		}
+	}
+}
+
