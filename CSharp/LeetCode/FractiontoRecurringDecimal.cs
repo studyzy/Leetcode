fe77caea08ad6354025a93d00d5881bcc2dde76f
Devin <studyzy@gmail.com>
2015-04-26 00:58:37 -0700

    add a medium test

---
+++ CSharp/LeetCode/FractiontoRecurringDecimal.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    public partial class Solution
+    {
+        public string FractionToDecimal(int numerator, int denominator)
+        {
+            bool negative = (numerator < 0 && denominator > 0) || (numerator > 0 && denominator < 0);
+
+            long numerator1 = numerator;
+            long denominator1 = denominator;
+            if (numerator < 0)
+            {
+                numerator1 = -numerator1;
+            }
+            if (denominator < 0)
+            {
+                denominator1 = -denominator1;
+            }
+            long zs = numerator1/denominator1;
+            long m =(numerator1 % denominator1);
+           
+            if (m== 0)
+            {
+                return (negative ? "-" : "") + zs.ToString();
+            }
+            var ysList = new List<long>();
+            ysList.Add(m);
+            StringBuilder sb=new StringBuilder();
+            int startIdx = 0;
+            while (true)
+            {
+                m = m*10;
+                var n =( m/denominator1);
+                sb.Append(n.ToString());
+                m =( m%denominator1);
+                if (ysList.Contains(m))
+                {
+                    startIdx = ysList.IndexOf(m);
+                    break;
+                }
+                if (m == 0) //Not Recurring
+                {
+                    return (negative?"-":"")+  zs + "." + sb.ToString();
+                }
+                ysList.Add(m);
+            }
+            var xs = sb.ToString();
+            return (negative ? "-" : "") + zs + "." + xs.Substring(0, startIdx) + "(" + xs.Substring(startIdx) + ")";
+       
+        
+        }
+    }
+}
---
+++ CSharp/LeetCodeUT/FractiontoRecurringDecimalUT.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace LeetCodeUT
+{
+    class FractiontoRecurringDecimalUT:BaseUT
+    {
+        [TestCase(1,7,"0.(142857)")]
+        [TestCase(6, 7, "0.(857142)")]
+        [TestCase(1, 3, "0.(3)")]
+        [TestCase(2, 3, "0.(6)")]
+        [TestCase(0, 3, "0")]
+        [TestCase(7, 5, "1.4")]
+        [TestCase(-50, 8, "-6.25")]
+        [TestCase(1, 97, "0.(010309278350515463917525773195876288659793814432989690721649484536082474226804123711340206185567)")]
+        [TestCase(7, -12, "-0.58(3)")]
+        [TestCase(12345, 410, "30.1(09756)")]
+        [TestCase(-2147483648, -1, "2147483648")]
+        [TestCase(-1, -2147483648, "0.0000000004656612873077392578125")]
+        public void Test(int n1, int n2, string result)
+        {
+            var m = Solution.FractionToDecimal(n1, n2);
+            Debug.WriteLine(m);
+            Assert.AreEqual(m, result);
+        }
+    }
+}
