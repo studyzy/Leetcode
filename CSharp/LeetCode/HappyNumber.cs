d60ac338a0fb4c7d1f5975c408e6e1b4357475e0
Devin <studyzy@gmail.com>
2015-04-28 23:18:53 -0700

    add HappyNumber test

---
+++ CSharp/LeetCode/HappyNumber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+	public partial class Solution 
+	{
+		public bool IsHappy(int n) 
+		{
+			List<int> list = new List<int> ();
+			list.Add (n);
+			while (true)
+			{
+				int total = 0;
+				foreach (char c in n.ToString())
+				{
+					int d=	c-'0';
+					total += d * d;
+				}
+				if (total == 1)
+					return true;
+				if (list.Contains (total))
+				{
+					return false;
+				}
+				list.Add (total);
+				n = total;
+			}
+
+		}
+	}
+}
+
---
+++ CSharp/LeetCodeUT/HappyNumberUT.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace LeetCodeUT
+{
+	public class HappyNumberUT:BaseUT
+	{
+		[TestCase(19,true)]
+		public void Test (int n,bool isHappy)
+		{
+			Assert.AreEqual (Solution.IsHappy (n), isHappy);
+		}
+	}
+}
+
