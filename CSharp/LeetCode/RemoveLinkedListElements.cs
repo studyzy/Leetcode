6da473ddeb49e8fc8a7b976130894b0a9c04b2af
Devin <studyzy@gmail.com>
2015-04-29 11:48:51 -0700

    an easy test

---
+++ CSharp/LeetCode/RemoveLinkedListElements.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    public partial class Solution
+    {
+        /// <summary>
+        /// https://leetcode.com/problems/remove-linked-list-elements/
+        /// 先找到Head，然后对剩下的元素for循环一次删除即可
+        /// </summary>
+        /// <param name="head"></param>
+        /// <param name="val"></param>
+        /// <returns></returns>
+        public ListNode RemoveElements(ListNode head, int val)
+        {
+
+            if (head == null)
+                return null;
+            while (head.val==val)
+            {
+                head = head.next;
+                if (head == null)
+                    return null;
+            }
+
+            ListNode p = head.next;
+            ListNode prev = head;
+            while (p!=null)
+            {
+
+                if (p.val == val)
+                {
+                    prev.next = p.next;
+                }
+                else
+                {
+                    prev = p;
+                }
+                p = p.next;
+            }
+            return head;
+        }
+    }
+}
---
+++ CSharp/LeetCodeUT/RemoveLinkedListElementsUT.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace LeetCodeUT
+{
+    class RemoveLinkedListElementsUT:BaseUT
+    {
+        [TestCase("1,2,3,4,5",4,"1,2,3,5")]
+        [TestCase("1,2,3,4,4,4,5", 4, "1,2,3,5")]
+        [TestCase("1,2,3,4,5", 5, "1,2,3,4")]
+        [TestCase("1,2,3,4,5", 1, "2,3,4,5")]
+        [TestCase("1,1,1", 1, "")]
+        [TestCase("1,2,3,4,5", 6, "1,2,3,4,5")]
+
+        public void Test(string list, int val, string newList)
+        {
+           var head=  BuildListNodes(list);
+            var head2 = Solution.RemoveElements(head, val);
+            var result = ListToString(head2);
+            Assert.AreEqual(result,newList);
+        }
+    }
+}
